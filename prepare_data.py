973d3cda84cabcaff4a6aad364a10f5acfd39b08
MICH <michel.blaauw@gmail.com>
2014-01-03 22:18:38 +0100

    v0.1

--- prepare_data.py
+++ prepare_data.py
@@ -1,1 +1,39 @@
-__author__ = 'MICH'
+
+#setup a standard image size; this will distort some images but will get everything into the same shape
+STANDARD_SIZE = (300, 300)
+def img_to_matrix(filename, verbose=False):
+    """
+    takes a filename and turns it into a numpy array of RGB pixels
+    """
+    img = Image.open(filename)
+    if verbose==True:
+        print "changing size from %s to %s" % (str(img.size), str(STANDARD_SIZE))
+    img = img.resize(STANDARD_SIZE)
+    img = list(img.getdata())
+    img = map(list, img)
+    img = np.array(img)
+    return img
+ 
+def flatten_image(img):
+    """
+    takes in an (m, n) numpy array and flattens it 
+    into an array of shape (1, m * n)
+    """
+    s = img.shape[0] * img.shape[1]
+    img_wide = img.reshape(1, s)
+    return img_wide[0]
+
+
+img_dir = "images/checks_and_dls/"
+images = [img_dir+ f for f in os.listdir(img_dir)]
+labels = ["check" if "check" in f.split('/')[-1] else "drivers_license" for f in images]
+
+data = []
+for image in images:
+    img = img_to_matrix(image)
+    img = flatten_image(img)
+    data.append(img)
+
+data = np.array(data)
+data
+
