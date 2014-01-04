4e2d07625b8d248d4d07d3ca8b9eb281cd0da7e2
MICH <michel.blaauw@gmail.com>
2014-01-04 11:16:33 +0100

    v0.1

--- prepare_data.py
+++ prepare_data.py
@@ -1,19 +1,34 @@
+import pandas as pd
+import numpy as np
+import pylab as pl
+from PIL import Image
+import os
+
+from sklearn.decomposition import RandomizedPCA
+from sklearn.neighbors import KNeighborsClassifier
 
 #setup a standard image size; this will distort some images but will get everything into the same shape
+
+# http://blog.yhathq.com/posts/image-classification-in-Python.html
+# http://nbviewer.ipython.org/gist/hernamesbarbara/5768969
+
 STANDARD_SIZE = (300, 300)
+
+
 def img_to_matrix(filename, verbose=False):
     """
     takes a filename and turns it into a numpy array of RGB pixels
     """
     img = Image.open(filename)
-    if verbose==True:
+    if verbose == True:
         print "changing size from %s to %s" % (str(img.size), str(STANDARD_SIZE))
     img = img.resize(STANDARD_SIZE)
     img = list(img.getdata())
     img = map(list, img)
     img = np.array(img)
     return img
- 
+
+
 def flatten_image(img):
     """
     takes in an (m, n) numpy array and flattens it 
@@ -24,8 +39,8 @@ def flatten_image(img):
     return img_wide[0]
 
 
-img_dir = "images/checks_and_dls/"
-images = [img_dir+ f for f in os.listdir(img_dir)]
+img_dir = "data/sample"
+images = [img_dir + f for f in os.listdir(img_dir)]
 labels = ["check" if "check" in f.split('/')[-1] else "drivers_license" for f in images]
 
 data = []
@@ -37,3 +52,27 @@ for image in images:
 data = np.array(data)
 data
 
+pca = RandomizedPCA(n_components=2)
+X = pca.fit_transform(data)
+df = pd.DataFrame({"x": X[:, 0], "y": X[:, 1], "label": np.where(y == 1, "Check", "Driver's License")})
+colors = ["red", "yellow"]
+for label, color in zip(df['label'].unique(), colors):
+    mask = df['label'] == label
+    pl.scatter(df[mask]['x'], df[mask]['y'], c=color, label=label)
+pl.legend()
+pl.show()
+
+pca = RandomizedPCA(n_components=5)
+train_x = pca.fit_transform(train_x)
+test_x = pca.transform(test_x)
+
+print train_x[:5]
+
+
+
+
+import pip
+from subprocess import call
+
+for dist in pip.get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
